ba06d883c20ec5c36ebcdc61d9988ae6bde76333
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-27 19:22:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,44 @@ from models.marketing_model import get_promotion_suggestions, configure_gemini_a
 from models.afinidad_model import get_related_products
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno
-load_dotenv()
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
-# Obtener la API Key de Gemini
-gemini_api_key = os.getenv('GEMINI_API_KEY')
-# Configurar la API generativa de Gemini
-configure_gemini_api()
+if GEMINI_API_KEY is None:
+    raise Exception("API key for Gemini not found. Make sure it's set in the config.toml file.")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+generation_config = {
+    "temperature": 0.4,
+    "top_p": 1,
+    "top_k": 32,
+    "max_output_tokens": 4096,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    }
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash-latest",
+    generation_config=generation_config,
+    safety_settings=safety_settings
+
 
 # Cargar el modelo de stock una vez al iniciar la aplicación
 try:
