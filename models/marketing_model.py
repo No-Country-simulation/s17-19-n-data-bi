63dfa3cf11d3740327a5717dadb236457eea0d2c
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-27 20:34:40 -0300

    Update marketing_model.py

--- models/marketing_model.py
+++ models/marketing_model.py
@@ -43,7 +43,7 @@ model = genai.GenerativeModel(
 )
 
 def get_promotion_suggestions(country, region, therapeutic_group):
-    prompt = f"Genera 10 recomendaciones de promociones sugeridas para el grupo terapéutico de {therapeutic_group} en {country}, {region}."
+    prompt = f"Genera posibles promociones recomendadas según los datos que el usuario ha ingresado, grupo terapéutico de {therapeutic_group} en {country}, {region}. Al menos 10."
 
     try:
         response = model.generate_content([prompt])
