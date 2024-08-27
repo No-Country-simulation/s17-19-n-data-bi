64f9d27516b3c75b542f0170d77b35c3dbc56ab9
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-26 23:22:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,18 @@ if gestion_stocks:
     model = load_model("stock")
 
     if model:
-        sucursal_id = st.text_input("Ingrese el ID de la sucursal")
-        skuagr_2 = st.text_input("Ingrese el SKU del producto")
+        if 'sucursal_id' not in st.session_state:
+            st.session_state['sucursal_id'] = ''
+        if 'skuagr_2' not in st.session_state:
+            st.session_state['skuagr_2'] = ''
+    
+        sucursal_id = st.text_input("Ingrese el ID de la sucursal", value=st.session_state['sucursal_id'])
+        skuagr_2 = st.text_input("Ingrese el SKU del producto", value=st.session_state['skuagr_2'])
 
         if st.button('Verificar Stock'):
+            st.session_state['sucursal_id'] = sucursal_id
+            st.session_state['skuagr_2'] = skuagr_2
+
             try:
                 input_data = torch.tensor([float(sucursal_id), float(skuagr_2)])
                 result = predict(model, input_data)
