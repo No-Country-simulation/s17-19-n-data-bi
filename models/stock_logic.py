1f7f25a2a71318a0e01c9d27af4263126879ece8
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-27 01:54:06 -0300

    Update stock_logic.py

--- models/stock_logic.py
+++ models/stock_logic.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import torch
 from models.inference import load_model, predict
+from models.stock_result import load_stock_data
 import pandas as pd
 
 def stock_verification():
@@ -49,26 +50,4 @@ def stock_verification():
                 st.error("Por favor ingrese valores numéricos válidos para la sucursal y SKU.")
             except Exception as e:
                 st.error(f"Error al verificar stock: {e}")
-
-def load_stock_data():
-    sucursales = pd.read_parquet('data/Sucursales.parquet')
-    productos = pd.read_parquet('data/Productos.parquet')
-    data = pd.read_parquet('data/Data.parquet')
-
-    stock_inicial = pd.merge(sucursales[['id_sucursal']], productos[['skuagr_2']], how='cross')
-    stock_inicial['stock_inicial'] = 100  # Asignar 100 unidades como stock inicial
-
-    transacciones_agrupadas = data.groupby(['id_sucursal', 'skuagr_2']).agg({'cantidad_dispensada': 'sum'}).reset_index()
-    stock_actualizado = pd.merge(stock_inicial, transacciones_agrupadas, on=['id_sucursal', 'skuagr_2'], how='left')
-    stock_actualizado['cantidad_dispensada'] = stock_actualizado['cantidad_dispensada'].fillna(0)
-    stock_actualizado['stock_disponible'] = stock_actualizado['stock_inicial'] - stock_actualizado['cantidad_dispensada']
-    stock_actualizado['hay_stock'] = stock_actualizado['stock_disponible'] > 0
-    stock_actualizado['hay_stock'] = stock_actualizado['hay_stock'].astype(int)
-
-    # Asegurarse de que las columnas tienen los tipos correctos
-    stock_actualizado['id_sucursal'] = stock_actualizado['id_sucursal'].astype(int)
-    stock_actualizado['skuagr_2'] = stock_actualizado['skuagr_2'].astype(str)
-
-    return stock_actualizado
-
 
