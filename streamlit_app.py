66b2d8236d322d8643da7f415a44b2ad79a31718
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-26 23:40:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,35 +43,8 @@ marketing_intelligence = st.sidebar.button('MARKETING INTELLIGENCE')
 afinidad_productos = st.sidebar.button('AFINIDAD DE PRODUCTOS')
 
 # Gestionar la lógica de cada botón
-if gestion_stocks:
-    st.title("Verificación de Stock en Sucursales")
-    model = load_model("stock")
-
-    if model:
-        
-        if 'sucursal_id' not in st.session_state:
-            st.session_state['sucursal_id'] = ''
-        if 'skuagr_2' not in st.session_state:
-            st.session_state['skuagr_2'] = ''
-
-        
-        sucursal_id = st.text_input("Ingrese el ID de la sucursal", value=st.session_state['sucursal_id'])
-        skuagr_2 = st.text_input("Ingrese el SKU del producto", value=st.session_state['skuagr_2'])
-
-        if st.button('Verificar Stock'):
-            
-            st.session_state['sucursal_id'] = sucursal_id
-            st.session_state['skuagr_2'] = skuagr_2
-
-            try:
-                
-                input_data = torch.tensor([float(sucursal_id), float(skuagr_2)])
-                result = predict(model, input_data)
-                st.write(f'Resultado de la predicción: {result}')
-            except ValueError:
-                st.error("Por favor ingrese valores numéricos válidos.")
-            except Exception as e:
-                st.error(f"Error al realizar la predicción: {e}")
+if st.sidebar.button('GESTIÓN DE STOCKS'):
+    stock_verification()
 
 if prevision_consumo:
     st.title("Selecciona el Método de Previsión")
