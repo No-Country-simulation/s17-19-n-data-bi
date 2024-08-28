16edd5a8fdb37dfc580676233fe6a2b1c9d9c155
Delicia Fedele Boria BackUP <defedeleboria@gmail.com>
2024-08-27 23:20:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ from models.marketing_model import get_promotion_suggestions
 from models.afinidad_model import get_affinity_recommendations
 from dotenv import load_dotenv
 
-
-GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
-print("GEMINI_API_KEY:", GEMINI_API_KEY)  # Agregar esta línea para verificar la clave
-except KeyError:
+if "GEMINI_API_KEY" in st.secrets:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+    print("GEMINI_API_KEY:", GEMINI_API_KEY)  # Verificar la clave
+else:
     raise Exception("API key for Gemini not found. Make sure it's set in the secrets.toml file.")
 
 genai.configure(api_key=GEMINI_API_KEY)
