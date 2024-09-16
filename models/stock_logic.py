314c7d2533f98a95a905586268753228b2e5b40a
Delicia Fedele Boria <defedeleboria@gmail.com>
2024-09-16 16:29:54 -0300

    Update stock_logic.py

--- models/stock_logic.py
+++ models/stock_logic.py
@@ -45,3 +45,26 @@ def show_stock_result(stock_data, id_sucursal, skuagr_2, model):
     
     # Mostrar los resultados
     st.write(f"Predicción de stock para SKU {skuagr_2} en sucursal {id_sucursal}: {prediction.item()}")
+
+def stock_verification():
+    # Cargar los datos una vez
+    stock_data = load_stock_data()
+
+    # Cargar el modelo de inferencia
+    model = load_model('stock')
+
+    # Crear el formulario para ingresar los datos
+    with st.form(key='stock_form'):
+        id_sucursal = st.text_input("Ingrese el ID de la sucursal", key="id_sucursal")
+        skuagr_2 = st.text_input("Ingrese el SKU del producto", key="skuagr_2")
+
+        # Botón para enviar el formulario
+        submit_button = st.form_submit_button(label='Verificar Stock')
+
+    # Verificar si se han ingresado datos válidos y mostrar resultados
+    if submit_button:
+        if id_sucursal and skuagr_2:
+            st.write("Verificando stock para Sucursal:", id_sucursal, "y SKU:", skuagr_2)
+            show_stock_result(stock_data, id_sucursal, skuagr_2, model)
+        else:
+            st.warning("Por favor, ingrese tanto el ID de la sucursal como el SKU del producto.")
